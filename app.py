b939ca2baec4198e550839357279516b76aad2ab
TimotheeNourris <66907341+TimotheeNourris@users.noreply.github.com>
2023-01-28 21:58:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ simple python flask application
 import ast
 import os
 import numpy as np
+import pandas as pd
 from flask import Flask
 from flask import request
 from flask import render_template
@@ -18,7 +19,7 @@ from flask.json import jsonify
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.datasets import fashion_mnist 
-
+import joblib
 
 ##########################################################################
 ## Application Setup
@@ -30,14 +31,15 @@ app = Flask(__name__)
 model = tf.keras.models.load_model("Nourris_Timothee_model_train.h5")
 # let's create a label for each
 class_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat', 'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
-# load datas from a csv file 
+
+# tries with loading datas from a csv file 
 '''train_images_test = pd.read_csv("train_images.csv")
 train_images_copy = train_images_test.drop(["label"], axis=1).copy()
 train_images_copy = train_images_copy.to_numpy()
 train_images_copy = train_images_copy.reshape(train_images_copy.shape[0], 28, 28)
 datas = {"pixels": {train_images_copy[0].to_list()}}'''
 
-# load datas from the mnist dataset
+# tries with loading datas from the mnist dataset
 '''(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 first_image = train_images[0]
 pixels = first_image.reshape(-1).tolist()
@@ -59,20 +61,22 @@ def hello():
     return jsonify({"hello": "world"})
     
 
-@app.route("/classify/<path:array>", methods=['GET', 'POST'])
+@app.route("/classify/<array>", methods=['GET', 'POST'])
 def classify(array):
     
-    image = ast.literal_eval(array)
+    image = json.loads(array, strict=False)
 
-    # preprocess the image
+    # reshape and normalization of the inputed image
     image = np.array(image, dtype=np.float32)
-    image = image.reshape(1, 28, 28, 1)
+    image = image.reshape(1, 28, 28)
     image = image.astype("float32")
     image /= 255
 
     # make the prediction
     prediction = model.predict(image)
+    #print(prediction)
     label = int(np.argmax(prediction))
+    #print(label)
     return jsonify({"prediction": label, 'label': class_labels[label]})
 
 ##########################################################################
